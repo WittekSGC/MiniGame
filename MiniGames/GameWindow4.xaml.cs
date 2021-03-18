074235c9ff2cf6a7101a22a8b43119eebd78dc43
WittekSGC <zoludovv@gmail.com>
2021-03-18 16:20:54 +0300

    game 4 - first step end (need testing)

--- MiniGames/GameWindow4.xaml.cs
+++ MiniGames/GameWindow4.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace MiniGames
 {
@@ -23,11 +16,20 @@ namespace MiniGames
         MainWindow Main;
         private bool ManualClosing = true;
         private Image[] StarsArray = new Image[5];
-        bool sized = true;
+        private int StarsEnabledCount = 0;
         int h, w;
-        int prevH, prevW;
-        int PrevClickGridSizeX, PrevClickGridSizeY;
+
         Point[] RightPosition = new Point[5];
+
+        //массив правильных точек нажатия
+        Image[][] ImagesForClicks = new Image[5][]
+        {
+            new Image[2],
+            new Image[2],
+            new Image[2],
+            new Image[2],
+            new Image[2]
+        };
 
         public GameWindow4(MainWindow main, WindowState state)
         {
@@ -36,40 +38,27 @@ namespace MiniGames
             WindowState = state;
             AddStars();
 
-            //SizeChanged += GameWindow4_SizeChanged;
-            //StateChanged += GameWindow4_StateChanged;
             Loaded += GameWindow4_Loaded;
 
-            RightPosition[0].X = 0;
+            RightPosition[0].X = 290;
             RightPosition[0].Y = 45;
 
-            RightPosition[1].X = 174;
-            RightPosition[1].Y = 76;
+            RightPosition[1].X = 105;
+            RightPosition[1].Y = 35;
 
-            RightPosition[2].X = 48;
-            RightPosition[2].Y = 176;
+            RightPosition[2].X = 65;
+            RightPosition[2].Y = 185;
 
-            RightPosition[3].X = 234;
-            RightPosition[3].Y = 58;
-            
-            RightPosition[4].X = 80;
-            RightPosition[4].Y = 52;
+            RightPosition[3].X = 225;
+            RightPosition[3].Y = 70;
 
+            RightPosition[4].X = 0;
+            RightPosition[4].Y = 20;
         }
 
         private void GameWindow4_Loaded(object sender, RoutedEventArgs e)
         {
             AddClickGrid();
-        }
-
-        private void GameWindow4_StateChanged(object sender, EventArgs e)
-        {
-            ClickGridReSize();
-        }
-
-        private void GameWindow4_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            ClickGridReSize();
         }
 
         /// <summary>
@@ -79,24 +68,66 @@ namespace MiniGames
         /// <param name="e">события объекта</param>
         private void R_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            double animationTime = 0.4;
             Image r = (sender as Canvas).Children[0] as Image;
+            bool Entry = false;
+
+            //проверяет, было ли нажатие на одну из двух картинок для кликов
+            foreach (Image[] items in ImagesForClicks)
+            {
+                foreach (Image item in items)
+                {
+                    if (r.Equals(item))
+                    {
+                        Entry = true;
+                        break;
+                    }
+                }
+                if (Entry) break;
+            }
+
+            //если NULL, значит было, а значит повторять анимации не нужно
+            if (!Entry) return;
+
+            //иначе проиграть анимации
+            double animationTime = 0.4;
 
             BitmapImage imgSrc = new BitmapImage();
             imgSrc.BeginInit();
             imgSrc.UriSource = new Uri("/Resources/Game4/circled.png", UriKind.Relative);
             imgSrc.EndInit();
-            r.Source = imgSrc;
 
             DoubleAnimation doubleAnimationW = new DoubleAnimation(0, w, TimeSpan.FromSeconds(animationTime));
             DoubleAnimation doubleAnimationH = new DoubleAnimation(0, h, TimeSpan.FromSeconds(animationTime));
-            r.BeginAnimation(WidthProperty, doubleAnimationW);
-            r.BeginAnimation(HeightProperty, doubleAnimationH);
-
-            ThicknessAnimation thicknessAnimation = new ThicknessAnimation(new Thickness(w / 2, h / 2, 0, 0), 
-                new Thickness(0, 0, 0, 0), 
+            ThicknessAnimation thicknessAnimation = new ThicknessAnimation(new Thickness(w / 2, h / 2, 0, 0),
+                new Thickness(0, 0, 0, 0),
                 TimeSpan.FromSeconds(animationTime));
-            r.BeginAnimation(MarginProperty, thicknessAnimation);
+
+            //и убрать обе картинки из массива
+            foreach (Image[] items in ImagesForClicks)
+            {
+                foreach (Image item in items)
+                {
+                    if (r.Equals(item))
+                    {
+                        items[0].Source = imgSrc;
+                        items[0].BeginAnimation(WidthProperty, doubleAnimationW);
+                        items[0].BeginAnimation(HeightProperty, doubleAnimationH);
+                        items[0].BeginAnimation(MarginProperty, thicknessAnimation);
+                        items[1].Source = imgSrc;
+                        items[1].BeginAnimation(WidthProperty, doubleAnimationW);
+                        items[1].BeginAnimation(HeightProperty, doubleAnimationH);
+                        items[1].BeginAnimation(MarginProperty, thicknessAnimation);
+
+                        items[0] = null;
+                        items[1] = null;
+
+                        //зажигание звезды 
+                        EndOfLevel();
+
+                        return;
+                    }
+                }
+            }
 
         }
 
@@ -118,64 +149,14 @@ namespace MiniGames
         }
 
         /// <summary>
-        /// метод, масштабирующий сетку нажатий под текущий размер экрана
-        /// </summary>
-        private void ClickGridReSize()
-        {
-            if (!sized)
-            {
-                sized = true;
-                return;
-            }
-            else
-            {
-                sized = false;
-            }
-
-            int counter = 0;
-            foreach (Canvas item in ClickGrid.Children)
-            {
-                item.Width = ClickGrid.Width / w * PrevClickGridSizeX;
-                item.Height = ClickGrid.Height / h * PrevClickGridSizeY;
-
-                //доработать отступы
-
-                item.Margin = new Thickness(item.Margin.Left * (PrevClickGridSizeX / ClickGrid.Width), 1, 0, 0);
-
-                foreach (UIElement child in item.Children)
-                {
-                    if (child is Image)
-                    {
-                        (child as Image).Width = item.Width;
-                        (child as Image).Height = item.Height;
-                        (child as Image).Margin = new Thickness(0);
-                        (child as Image).SetCurrentValue(WidthProperty, item.Width);
-                        (child as Image).SetCurrentValue(HeightProperty, item.Height);
-                        (child as Image).SetCurrentValue(MarginProperty, new Thickness(0));
-                    }
-                    prevW = (int)item.Width;
-                    prevW = (int)item.Height;
-                }
-
-                counter++;
-            }
-
-            ClickGrid.UpdateLayout();
-            PrevClickGridSizeX = (int)ClickGrid.Width;
-            PrevClickGridSizeY = (int)ClickGrid.Height;
-        }
-
-        /// <summary>
         /// добавление сетки для нажатий
         /// </summary>
         private void AddClickGrid()
         {
             w = 40;
             h = 40;
-            prevW = 40;
-            prevH = 40;
-            PrevClickGridSizeX = (int)ClickGrid.Width;
-            PrevClickGridSizeY = (int)ClickGrid.Height;
+            int countX = 360 / w;
+            int countY = 258 / h;
 
             for (int i = 0; i < 5; i++)
             {
@@ -202,39 +183,42 @@ namespace MiniGames
                 int marginTop = (int)RightPosition[i].X;
                 int marginLeft = (int)RightPosition[i].Y;
                 c.Margin = new Thickness(marginTop, marginLeft, 0, 0);
-                ClickGrid.Children.Add(c);
-            }
-            /*for (int i = 0; i < 10; i++)
-            {
-                for (int j = 0; j < 10; j++)
-                {
-                    Canvas c = new Canvas()
-                    {
-                        Width = w,
-                        Height = h,
-                        HorizontalAlignment = HorizontalAlignment.Left,
-                        VerticalAlignment = VerticalAlignment.Top,
-                        Background = Brushes.Transparent
-                    };
-                    Image r = new Image()
-                    {
-                        Source = null,
-                        Width = 0,
-                        Height = 0,
-                        Margin = new Thickness(w / 2, h / 2, 0, 0),
-                        HorizontalAlignment = HorizontalAlignment.Center,
-                        VerticalAlignment = VerticalAlignment.Center
-                    };
+                LeftClicks.Children.Add(c);
 
-                    c.Children.Add(r);
-                    c.MouseLeftButtonUp += R_MouseLeftButtonUp;
-                    int marginTop = i * w;
-                    int marginLeft = j * h;
-                    c.Margin = new Thickness(marginTop, marginLeft, 0, 0);
-                    ClickGrid.Children.Add(c);
-                }
+                //занесение созданной картинки в массив 
+                ImagesForClicks[i][0] = r;
             }
-            */
+
+            for (int i = 0; i < 5; i++)
+            {
+                Canvas c = new Canvas()
+                {
+                    Width = w,
+                    Height = h,
+                    HorizontalAlignment = HorizontalAlignment.Left,
+                    VerticalAlignment = VerticalAlignment.Top,
+                    Background = Brushes.Transparent
+                };
+                Image r = new Image()
+                {
+                    Source = null,
+                    Width = 0,
+                    Height = 0,
+                    Margin = new Thickness(w / 2, h / 2, 0, 0),
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center
+                };
+
+                c.Children.Add(r);
+                c.MouseLeftButtonUp += R_MouseLeftButtonUp;
+                int marginTop = (int)RightPosition[i].X;
+                int marginLeft = (int)RightPosition[i].Y;
+                c.Margin = new Thickness(marginTop, marginLeft, 0, 0);
+                RightClicks.Children.Add(c);
+
+                //занесение созданной картинки в массив 
+                ImagesForClicks[i][1] = r;
+            }
         }
 
         /// <summary>
@@ -259,6 +243,50 @@ namespace MiniGames
                 };
 
                 StarsPanel.Children.Add(StarsArray[i]);
+            }
+        }
+
+        /// <summary>
+        /// зажигание звездочки при правильном нажатии
+        /// </summary>
+        private void EndOfLevel()
+        {
+            DoubleAnimation showStar = new DoubleAnimation(1, TimeSpan.FromSeconds(0.6));
+            StarsArray[StarsEnabledCount].BeginAnimation(OpacityProperty, showStar);
+            StarsEnabledCount++;
+
+            if (StarsEnabledCount == 5)
+            {
+                EndOfGame();
+            }
+        }
+
+        /// <summary>
+        /// метод, выполняющийся когда все отличия найдены
+        /// </summary>
+        private void EndOfGame()
+        {
+            bool? Result = new ModalWindow("Молодец! Хочешь сыграть еще раз?", ModalWindowMode.TextWithYesNoBtn).ShowDialog();
+            if ((bool)Result)
+            {
+                //восстанвить начальные значения 
+
+                StarsEnabledCount = 0;
+                foreach (Image item in StarsArray)
+                {
+                    item.Opacity = 0.4;
+                }
+
+                LeftClicks.Children.Clear();
+                RightClicks.Children.Clear();
+
+                AddClickGrid();
+            }
+            else
+            {
+                ManualClosing = false;
+                Close();
+                return;
             }
         }
     }
